347dc68b1cbe8ee5257c23befdc980c40f9c1056
Aaron cabrales <79927983+aaroncabc@users.noreply.github.com>
2025-08-29 18:27:58 -0500

    Add files via upload

---
+++ taller02.js
@@ -0,0 +1,52 @@
+function findMax(arr){
+    let max = 0
+    arr.forEach(e => {
+        max = e>max? e: max
+    })
+    return max
+}
+
+function findMin(arr){
+    let min = arr[0]
+    arr.forEach(e => {
+        min = e<min? e: min
+    })
+    return min
+}
+
+function includes(arr,val){
+    for (let e of arr){
+        if (e === val) return true
+    }
+    return false
+}
+
+function sum(arr){
+    cont = 0
+    for (let e of arr){
+        cont += e
+    }
+    return cont
+}
+
+function missingNumbers(arr){
+    start = findMin(arr)
+    finish = findMax(arr)
+    faltantes = []
+    for (let i = start; i < finish; i++) {
+        if(!includes(arr, i)){
+            faltantes.push(i)
+        }
+    }
+    return faltantes
+}
+
+
+Numeros = [3,10,-1,4,-5]
+console.log(findMax(Numeros));
+console.log(findMin(Numeros));
+console.log(includes(Numeros,2));
+console.log(sum(Numeros));
+
+console.log(missingNumbers(Numeros));
+
